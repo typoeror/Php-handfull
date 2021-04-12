04d65e066632e86a5e5387443736b25c56757d9e
typoeror <59331912+typoeror@users.noreply.github.com>
2021-04-12 15:57:49 +0530

    Create encrypt-decrypt-string-using-a-private-secret-key.php

---
+++ encrypt-decrypt-string-using-a-private-secret-key.php
@@ -0,0 +1,29 @@
+<?php
+  function encrypt($message, $encryption_key){
+    $key = hex2bin($encryption_key);
+    $nonceSize = openssl_cipher_iv_length('aes-256-ctr');
+    $nonce = openssl_random_pseudo_bytes($nonceSize);
+    $ciphertext = openssl_encrypt(
+      $message, 
+      'aes-256-ctr', 
+      $key,
+      OPENSSL_RAW_DATA,
+      $nonce
+    );
+    return base64_encode($nonce.$ciphertext);
+  }
+  function decrypt($message,$encryption_key){
+    $key = hex2bin($encryption_key);
+    $message = base64_decode($message);
+    $nonceSize = openssl_cipher_iv_length('aes-256-ctr');
+    $nonce = mb_substr($message, 0, $nonceSize, '8bit');
+    $ciphertext = mb_substr($message, $nonceSize, null, '8bit');
+    $plaintext= openssl_decrypt(
+      $ciphertext, 
+      'aes-256-ctr', 
+      $key,
+      OPENSSL_RAW_DATA,
+      $nonce
+    );
+    return $plaintext;
+  }
